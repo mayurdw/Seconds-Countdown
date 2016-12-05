9c32b108a7b31369e932f28297cc3fc395682784
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2016-12-05 23:41:18 +1300

    revision one

---
+++ countdown.js
@@ -0,0 +1,27 @@
+//global variables
+var numberValue = document.getElementById("area");
+var intervalHandle;
+//To ensure that textbox is null when start
+function startUp() {
+document.getElementById("area").value = " ";
+}
+//To count to zero
+function startCount() {
+  if(numberValue.value > 0) {
+    numberValue.value--;
+  } else{
+  clearInterval(intervalHandle);
+  alert("Stop");
+  startUp();
+  }
+}
+//main program
+startUp();
+document.getElementById("start").onclick = function(){
+  if(isNaN(numberValue.value)){
+    alert("enter number only!");
+    return;
+  }else{
+    intervalHandle = setInterval(startCount,1000);
+    }
+};
